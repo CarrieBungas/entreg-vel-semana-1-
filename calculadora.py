a7bfa4e1a1fe2e5cf3527ba53be94a29cd80f2df
CarrieBungas <arthurcesar987654321@gmail.com>
2026-09-19 20:25:43 -0300

    Add files via upload

---
+++ calculadora.py
@@ -0,0 +1,9 @@
+valor_compra = float(input("Digite o valor da compra: "))
+valor_pago = float(input("Digite o valor pago: "))
+
+troco = valor_pago - valor_compra
+
+print(f"Troco: R$ {troco:.2f}")
+
+
+
